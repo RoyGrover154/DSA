de45247eaac89a4098cb2b3bcd6dde0eee85ebaa
Gaurav Mani <142533365+RoyGrover154@users.noreply.github.com>
2025-06-07 00:41:32 +0530

    Time: 7 ms (31.22%) | Memory: 12 MB (29.60%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -4,18 +4,13 @@ public:
       
         unordered_map<char ,int> count;
 
-        for(char ch : magazine){
-            if(count.find(ch) == count.end()){
-                count[ch] = 1;
-            }
-            else{
-                count[ch]++;
-            }
-        }
+       for(char ch: magazine){
+        count[ch]++;
+       }
+        
         for(char ch : ransomNote){
-            if(count.find(ch) != count.end() && count[ch] > 0){
+            if(count[ch] > 0){
                 count[ch]--;
-
             }
             else{
                 return false;
