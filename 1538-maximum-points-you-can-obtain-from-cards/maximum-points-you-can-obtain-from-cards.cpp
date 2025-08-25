8a2e176da1ccc12a75f5cfe83ee3e8b95609184f
Gaurav Mani <142533365+RoyGrover154@users.noreply.github.com>
2025-08-25 21:55:30 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (46.89%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
-        int n = cardPoints.size();
-        int maxcount = 0;
-        int leftsum = 0;
-        int rightsum =0;
+     int n = cardPoints.size();
+     int maxcount = 0;
+     int leftsum = 0;
+     int rightsum = 0;
 
-        for(int i=0; i<k; i++){
-            leftsum = leftsum + cardPoints[i];
-            maxcount = leftsum;
-        }
+     for(int j=0; j<k; j++){
+        leftsum += cardPoints[j];
+        maxcount = leftsum;
+     }
+     
+     int idx = n-1;
 
-        int rightidx = n-1;
+   for(int i=k-1; i>=0; i--){
+    leftsum = leftsum - cardPoints[i];
+    rightsum = rightsum + cardPoints[idx];
+    idx = idx -1;
 
-        for(int i=k-1; i>=0; i--){
-          leftsum = leftsum - cardPoints[i];
-          rightsum = rightsum + cardPoints[rightidx];
-          rightidx =rightidx - 1;
-        
-     maxcount = max(maxcount , leftsum + rightsum);
-        }
-        return maxcount;
-    }
+    maxcount = max(maxcount, rightsum + leftsum);
+   }
+   return maxcount;
+   }
+
 };
