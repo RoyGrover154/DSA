cd9b087b34b27619eaf27a7999e4df38803cddf6
Gaurav Mani <142533365+RoyGrover154@users.noreply.github.com>
2025-09-03 00:53:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (18.42%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+          
+          int n = s.length();
+         stack<int>s1;
+         stack<int>s2;
+
+        for(int i=0; i<n ;i++){
+            if(s[i] == '('){
+            s1.push(i);
+            }
+            else if(s[i] == '*'){
+                s2.push(i);
+        }
+        else{
+            if(!s1.empty()){
+                s1.pop();
+            }
+        else if(!s2.empty()){
+            s2.pop();
+        }
+        else{
+            return false;
+        }
+        }
+
+    }
+        while(!s1.empty() && !s2.empty()){
+            if(s1.top() > s2.top()){
+                return false;
+            }
+
+            s1.pop();
+            s2.pop();
+
+
+        }
+    return s1.empty();
+    }
+};
