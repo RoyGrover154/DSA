9abd8a1515b846bbcbf9c4fc8658a2a32c8e653b
Gaurav Mani <142533365+RoyGrover154@users.noreply.github.com>
2025-09-03 01:01:18 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (92.75%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,41 +1,71 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-          
           int n = s.length();
-         stack<int>s1;
-         stack<int>s2;
+          int low= 0;
+          int high = 0;
 
-        for(int i=0; i<n ;i++){
-            if(s[i] == '('){
-            s1.push(i);
+          for(char c : s){
+            if(c == '('){
+                low++;
+                high++;
             }
-            else if(s[i] == '*'){
-                s2.push(i);
-        }
-        else{
-            if(!s1.empty()){
-                s1.pop();
+            else if(c == ')' ){
+                low--;
+                high--;
             }
-        else if(!s2.empty()){
-            s2.pop();
-        }
-        else{
-            return false;
-        }
-        }
+            else{
+                low--;
+                high++;
+            }
+
+            if(high < 0) return false;
+            if(low < 0) low = 0;
+          }
+
+          return low==0;
 
     }
-        while(!s1.empty() && !s2.empty()){
-            if(s1.top() > s2.top()){
-                return false;
-            }
-
-            s1.pop();
-            s2.pop();
+    };
 
 
-        }
-    return s1.empty();
-    }
-};
+
+
+          ////stack ///
+//           int n = s.length();
+//          stack<int>s1;
+//          stack<int>s2;
+
+//         for(int i=0; i<n ;i++){
+//             if(s[i] == '('){
+//             s1.push(i);
+//             }
+//             else if(s[i] == '*'){
+//                 s2.push(i);
+//         }
+//         else{
+//             if(!s1.empty()){
+//                 s1.pop();
+//             }
+//         else if(!s2.empty()){
+//             s2.pop();
+//         }
+//         else{
+//             return false;
+//         }
+//         }
+
+//     }
+//         while(!s1.empty() && !s2.empty()){
+//             if(s1.top() > s2.top()){
+//                 return false;
+//             }
+
+//             s1.pop();
+//             s2.pop();
+
+
+//         }
+//     return s1.empty();
+//     }
+// };
