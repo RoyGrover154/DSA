e9a053abba492dfb7029d1819be6d46b664ac7ca
Gaurav Mani <142533365+RoyGrover154@users.noreply.github.com>
2025-09-06 02:07:04 +0530

    Time: 0 ms (100.00%) | Memory: 22.2 MB (80.25%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+       int n = ratings.size();
+        int sum = 1;
+        int i = 1;
+
+        while(i < n){
+            if(ratings[i] == ratings[i-1]){
+                sum = sum+1;
+                i++;
+                continue;
+            }
+
+            int peak = 1;
+
+            while(i< n && ratings[i] > ratings[i-1]){
+                peak += 1;
+                sum += peak;
+                i++;
+            }
+            
+            int down = 1;
+
+            while(i<n && ratings[i] < ratings[i-1]){
+                sum = sum + down;
+                i++;
+                down += 1;
+            }
+
+            if(down > peak){
+                sum = sum + down - peak;
+            }
+        }
+        return sum;
+    }
+};
