0702199ea1cbc91b50ac0b8e8a3eec6eab081f99
Gaurav Mani <142533365+RoyGrover154@users.noreply.github.com>
2025-10-01 23:12:38 +0530

    Time: 6 ms (5.28%) | Memory: 16.9 MB (6.01%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,43 +1,29 @@
 class Solution {
 public:
-    // vector<vector<int>> ans;
 
-    // void solve(vector<int>& nums, int index, vector<int>& temp){
-    //     if(index >= nums.size()){
-    //         ans.push_back(temp);
-    //         return;
-    //     } 
-        
-    //     temp.push_back(nums[index]);
-    //     solve(nums, index+1, temp);
-    //     temp.pop_back();
-    //     solve(nums, index+1, temp);
-    // }
+    void check(vector<vector<int>>&ans, vector<int>nums, int index, vector<int>temp){
+
+        if(index == nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+
+        check(ans, nums, index+1, temp);
+        temp.push_back(nums[index]);
+        check(ans,nums,index+1,temp);
+        temp.pop_back();
+            
+        }
+
     
 
+
     vector<vector<int>> subsets(vector<int>& nums) {
-        // vector<int> temp; // store subset;
         
-        // solve(nums, 0, temp);
+        vector<vector<int>>ans;
+        vector<int>temp;
+       check(ans, nums, 0,temp);
 
-        int n = nums.size();
-
-        int subset = 1 << n;
-
-    vector<vector<int>> ans;
-
-    for(int i=0; i < subset; i++){
-        vector<int> list;
-
-        for(int j=0; j<n; j++){
-            if(i & (1 << j)){
-                list.push_back(nums[j]);
-            }
-        }
-        ans.push_back(list);
-    }
-
-    return ans;
-        
+       return ans;
     }
 };
