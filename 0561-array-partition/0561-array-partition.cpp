e631eee3c2bd4a1cbae464fc2c23d2d968325610
Gaurav Mani <142533365+RoyGrover154@users.noreply.github.com>
2025-10-23 10:10:22 +0530

    Time: 6 ms (73.86%), Space: 32 MB (54.83%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+
+
+        ////for greedy first apply sort
+        sort(nums.begin(), nums.end());
+
+        int sum = 0;
+
+        
+        for(int i=0; i<nums.size(); i=i+2){
+            sum = sum + nums[i];
+        }
+
+        return sum;
+    }
+};
